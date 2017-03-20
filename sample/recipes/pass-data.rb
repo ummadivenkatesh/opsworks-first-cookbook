4c0af3e9c07ab6f33ab062cd4b818bd7956baee7
ummadivenkatesh <1601010267@svuca.edu>
2017-03-20 13:39:05 -0700

    Update pass-data.rb

--- sample/recipes/pass-data.rb
+++ sample/recipes/pass-data.rb
@@ -1,4 +1,4 @@
-node[:deploy].each do |app, deploy|
+node[:deploy] do
   file File.join(deploy[:deploy_to], 'shared', 'config', 'app_data.yml') do
     content YAML.dump(node[:my_app_data][app].to_hash)
   end
